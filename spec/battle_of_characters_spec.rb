c599c4bafbd3aee8aa94fce0acb0c1bc220f826a
Haroon <haroon20@hotmail.co.uk>
2022-03-07 23:11:54 +0000

    A to Z hash added

--- spec/battle_of_characters_spec.rb
+++ spec/battle_of_characters_spec.rb
@@ -6,5 +6,9 @@ describe Battle do
             battle = Battle.new
             expect(battle.score('A')).to eq(1)
         end
+        it 'Should return a score of 1 for the string A' do
+            battle = Battle.new
+            expect(battle.score('B')).to eq(2)
+        end
     end
 end
