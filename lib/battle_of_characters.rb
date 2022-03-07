60c5e43816533ed92e98cfea7fa2a9b5c13225cc
Haroon <haroon20@hotmail.co.uk>
2022-03-07 23:10:15 +0000

    Hash for A to Z complete

--- lib/battle_of_characters.rb
+++ lib/battle_of_characters.rb
@@ -1,5 +1,41 @@
 class Battle
-    def score(x)
-        return 1
+    def initialize
+        @count = 1
+        @letter_num_hash = {}
     end
+
+    def letter_score
+        ("A".."Z").each do |letter|
+            @letter_num_hash[letter] = @count
+            @count = @count + 1
+           
+        end
+
+    puts @letter_num_hash
+    end
+
+    
+
+    # def score(x)
+    #     if x
+
+    # end
 end
+
+battle = Battle.new
+return battle.letter_score
+
+
+# class Abc
+#     def initialize
+#         @a = 0
+#     end
+#     def asd
+#         @a = 2+2
+#         puts @a
+#     end
+    
+# end
+
+# qwerty = Abc.new
+# qwerty.asd
